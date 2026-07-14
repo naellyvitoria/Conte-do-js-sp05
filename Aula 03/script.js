70a8b5de7a65d9070bdb01cc909926881f238fc5
Naelly <naellyv61@gmail.com>
2026-07-14 18:53:57 -0300

    Aula 03

---
+++ Aula 03/script.js
@@ -0,0 +1,113 @@
+// Declarando variáveis com o let
+let carro = "BMW";
+let moto = "CB500";
+let cor = "preto";
+
+console.log(carro);
+console.log(moto);
+console.log(cor);
+
+console.log("-----------------");
+
+// Declarando variáveis com const
+const preco1 = 10;
+const preco2 = 20;
+let valorTotal = preco1 + preco2;
+
+console.log("O valor total é: " + valorTotal);
+
+console.log("-----------------")
+
+// Declarando variáveis de forma automática
+x = 1;
+y = 6;
+z = x + y;
+
+console.log(z);
+
+console.log("");
+
+// Declarando variáveis com var
+
+var fruta1 = "Abacate";
+var fruta2 = "Maçã";
+var fruta3 = "Morango";
+
+console.log(fruta1);
+console.log(fruta2);
+console.log(fruta3);
+
+console.log("");
+
+// Tipos de dados em variáveis (números e strings)
+
+let numero1 = 4;
+let numero2 = 7;
+let numero3 = "8";
+let numero4 = "18";
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+
+let soma1 = numero1 + numero2;
+
+console.log(soma1);
+
+let soma2 = numero3 + numero4;
+
+console.log(soma2);
+
+console.log("");
+
+// Declarando variáveis em uma única instrução
+
+let nomePessoa = "Naelly Vitoria", 
+idade = 19, 
+cidade = "Carapicuiba";
+
+console.log(nomePessoa);
+console.log(idade);
+console.log(cidade);
+
+// let num1 = 5;
+
+// console.log(num1);
+
+// num1 = num1 + 5;
+
+// console.log(num1);
+
+console.log("");
+
+// Operações aritméticas
+
+let soma = 5 + 3 + 2;
+console.log(soma);
+
+let nomeConcatenado = "Naelly Vitoria";
+console.log(nomeConcatenado);
+
+console.log("");
+
+let valorPrincipal = 20;
+console.log(valorPrincipal);
+
+{
+  let valorPrincipal = 5;
+  console.log(valorPrincipal);
+}
+
+console.log(valorPrincipal);
+
+
+console.log("");
+
+const carro2 = "Fiat";
+
+console.log(carro2);
+
+{
+  console.log(carro2);
+}
---
+++ Aula 03/script02.js
@@ -0,0 +1,52 @@
+// Exemplos utilizando o typeof
+
+console.log(typeof "");
+console.log(typeof "56");
+console.log(typeof "Bom dia!");
+
+console.log("");
+
+console.log(typeof 0);
+console.log(typeof 28);
+console.log(typeof (4));
+
+// Exemplos de string
+
+console.log("");
+
+let primeiroNome = "Naelly";
+let segundoNome = "Vitoria";
+let nomeCompleto = "Naelly Vitoria";
+
+console.log("Primeiro nome: " + primeiroNome);
+console.log("Segundo nome: " + segundoNome);
+console.log("Nome completo: " + nomeCompleto);
+
+// Exemplos de number
+
+console.log("");
+
+let num1 = 0;
+let num2 = 80;
+let num3 = 190;
+
+console.log("Primeiro número: " + num1);
+console.log("Segundo número: " + num2);
+console.log("Terceiro número: " + num3);
+
+// Exemplos de boolean
+
+console.log("");
+
+let valor = 6;
+
+console.log("6 é maior que 7?: " + (valor > 7));
+console.log("6 é menor que 10?: " + (6  < 10));
+
+// Exemplo de undefined
+
+console.log("");
+
+let semValor;
+
+console.log(semValor);
